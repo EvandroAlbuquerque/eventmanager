5ffbb518afb815632105f846975a976fce3ef006
Evandro <evandrobotelhoalbuquerque@gmail.com>
2025-04-19 15:03:14 -0300

    Adiciona estrutura básica de processar inscrição.

--- inscricao/urls.py
+++ inscricao/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('processa_inscricao', views.processa_inscricao, name='processa_inscricao')
 ]
--- inscricao/views.py
+++ inscricao/views.py
@@ -1,5 +1,15 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from .models import Pessoa
 
 def index(request):
     return render(request, 'inscricao/index.html')
+
+def processa_inscricao(request):
+    nome = request.POST.get('nome')
+    email = request.POST.get('email')
+    
+    pessoa = Pessoa(nome=nome, email=email)
+    pessoa.save()
+    
+    return HttpResponse(f'Pessoa {nome}-{email} salva com sucesso!')
